a727e0b94475db081aa1da6a089bd8f3699c675e
Murray2015 <murrayhoggett@gmail.com>
2020-02-18 09:26:47 +0000

    First commit

---
+++ yourSolution.js
@@ -0,0 +1,22 @@
+/* Sudo-kool for School
+
+Having secured a place on the School of Code bootcamp, you are certain that your dream job in the tech industry is finally within sight. Your coding knowledge has increased beyond your wildest expectations, and your upper thigh muscles have never felt better. However, one morning a strange email appears in your inbox. It reads as follows:
+
+Dear Bootcamper,
+
+The International Sudoku Arbitration Directorate has been watching your progress carefully. We are delighted to be able to write to you with an offer of employment. As I am sure you are aware, millions of sudoku puzzles are completed every day. It is our sworn duty to ensure that they are checked for accuracy. To this end, we always ensure the best and brightest in our society are given the opportunity to fulfil this important role. All the details are attached. We look forward to welcoming you to the team as soon as possible.
+
+Yours faithfully
+Mao Shoggett,
+ISAD President
+
+You feel lightheaded after reading the email, and the words hazily float on the screen in front of you. You realise that you never wanted to be a programmer at all. Your real dream is checking the accuracy of sudoku puzzles, now and for all time. All that is left to do is put the few coding skills you have learnt towards this one objective, and your life will be complete.
+
+Task: write a function that takes in a completed sudoku puzzle, and returns true or false depending on whether it has been filled in correctly or incorrectly. The completed puzzle is made up of an array which contains nine other arrays. Each of these nine arrays rep resents a completed row of the sudoku grid, from top to bottom.
+ */
+
+function checkSudoku() {
+  // Add your code here!
+}
+
+module.exports = checkSudoku;
